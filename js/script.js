575d93a20bba196dc1ba824a7f87fe90fd6804c0
Erick <witherick11@gmail.com>
2025-01-25 14:52:32 -0600

    cambios 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const heroSection = document.querySelector('.hero');
-    heroSection.classList.add('fade-in');
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('open');
+    navLinks.classList.toggle('active');
 });
+
+
+// const carousel = document.querySelector('.carousel');
+// const prevBtn = document.querySelector('.carousel-btn.prev');
+// const nextBtn = document.querySelector('.carousel-btn.next');
+
+// let scrollAmount = 0;
+
+// // Botón de navegación izquierda
+// prevBtn.addEventListener('click', () => {
+//     carousel.scrollBy({
+//         left: -300, // Ajusta el desplazamiento
+//         behavior: 'smooth',
+//     });
+// });
+
+// // Botón de navegación derecha
+// nextBtn.addEventListener('click', () => {
+//     carousel.scrollBy({
+//         left: 300, // Ajusta el desplazamiento
+//         behavior: 'smooth',
+//     });
+// });
+
+
